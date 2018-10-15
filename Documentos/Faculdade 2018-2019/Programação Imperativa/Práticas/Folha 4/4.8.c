daf26e8797e84a59b50c34bf83d33a24e6109553
Fábio <figboy98>
2018-10-15 23:34:07 +0100

    Folha 4

--- Documentos/Faculdade 2018-2019/Programação Imperativa/Práticas/Folha 4/4.8.c
+++ Documentos/Faculdade 2018-2019/Programação Imperativa/Práticas/Folha 4/4.8.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+int main(){
+	int valor, aux, bin=0, multiplicador=1;
+	
+	printf("Escreva o número que quer passar para binário: ");
+	scanf("%d", &valor);
+	
+	while(valor!=0){
+		aux=valor%2;
+		bin+=aux*multiplicador;
+		multiplicador*=10;
+		valor/=2;
+	}
+	
+	printf("O numero decimal convertido para binário é: %d\n", bin);
+		
 
+	return 0;
+}
+
+			
+		
