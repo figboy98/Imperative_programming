54deee1ec746d104f45998ea7f7fa45a2fa29cd2
Fábio <figboy98>
2018-11-06 19:10:06 +0000

    Folha 6 completa

--- Folha 6/6.9.c
+++ Folha 6/6.9.c
@@ -1,35 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define N 100
-int main(){
-	int aleatorio;
-	int vec[N];
-	
-	srand((unsigned)time(NULL));
-	
+#define N 367
+#define experiencias 10000
+int repetidos(int vec[]){
+	//int probabilidade;
 	for(int i=0; i<N; i++){
-		aleatorio= 1+ (rand()/RAND_MAX/365 +1);
-		vec[i]=aleatorio;
-	}
-	
-	if(
-	
-	
-	
-	
-	
-int repetidos(int vec[], int size){
-	int i, j;
-		
-	for(i=0; i<size;i++){
-		for(j=i+1; j<size; j++){
-		if(vec[i]==vec[j]){
-			return 1;
+		for(int j=i+1; j<N; j++){
+			if(vec[i] == vec[j]){
+				return 1;
+			}
 		}
 	}
 }
 
-	return 0;
-}
+int main(){
 	
+	float contador=0;
+	float probabilidade;
+	int vec[N];
+	int aleatorio;
+	srand((unsigned)time(NULL));
+
+	for(int i=0; i<experiencias; i++){
+		
+		for( int j=0; j<N; j++){
+			aleatorio= 1+rand()/(RAND_MAX/365 +1);
+			vec[j] = aleatorio;
+		}
+		
+		if(repetidos(vec) ==1){
+			contador++;
+		}
+		probabilidade = (contador/experiencias);
+	}
+	printf("Probabilidade: %f\n", probabilidade);
+
+
+}
