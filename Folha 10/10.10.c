7582eccc8dbd435eb5003572e2d8505c116c2553
figboy98 <f.m.g.silva@hotmail.com>
2018-12-10 11:00:01 +0000

    10.10 inc

---
+++ Folha 10/10.10.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int comparar( char *str1, char *str2){
+	if(strlen(str1) != strlen(str2)){
+		return 0;
+	}
+		
+		
+	while(*str1 != '\0'){
+		if(*str1 != *str2){
+			return 0;
+		}
+		str1++;
+		str2++;
+	}
+	return 1;
+}
+
+int main(){
+	char str1[100]="Hello World";
+	char str2[100]="Hello World";
+	
+	int aux=(comparar(str1, str2);
+	
+	printf("%d\n", aux);
+	return 0;
+}
+
